02f21feb63ec58081733f2ba18eab79cf6708396
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-06-25 23:24:23 +0530

    feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import FeatureSection from './components/FeatureSection'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <Hero/>
+        <FeatureSection/>
       </div>
     </>
   )
