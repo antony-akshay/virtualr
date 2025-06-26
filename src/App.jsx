74dc09e956c5346648b85c4b358d1f775c4d1b3a
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-06-26 13:25:33 +0530

    workflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import FeatureSection from './components/FeatureSection'
+import Workflow from './components/Workflow'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <Hero/>
         <FeatureSection/>
+        <Workflow/>
       </div>
     </>
   )
